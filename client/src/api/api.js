0fe7754dd28ba9b8d6eceebbb0a570f3580f20a4
Alexander Bibikov <bibikov.a@live.ru>
2021-02-04 13:27:00 +0300

    production fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,8 @@ import * as axios from 'axios'
 
 const instance = axios.create({
 	withCredentials: true,
-	baseURL: 'http://localhost:5000/api/',
+	//baseURL: 'http://localhost:5000/api/',
+	baseURL: 'http://xn--80ab0aeodhol1g.xn--p1ai/',
 })
 
 export const appAPI = {
